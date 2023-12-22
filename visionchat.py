3edb4e29217b7e6a2b55f5e3764691d86215de89
ram kumar <rumk78864@gmail.com>
2023-12-23 00:09:13 +0530

    image chat

--- visionchat.py
+++ visionchat.py
@@ -4,18 +4,21 @@ load_dotenv()  # to load all the environment variables
 import streamlit as st
 import os
 import google.generativeai as genai
+from PIL import Image
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 # Loading gemini pro model
-model = genai.GenerativeModel("gemini-pro")
+model = genai.GenerativeModel("gemini-pro-vision")
 
-# Function to get response from model
-def get_response(query):
-    response = model.generate_content(query)
+def get_response(prompt, image):
+    if prompt!="":
+        response=model.generate_content([prompt,image])
+    else:
+        response=model.generate_content(image)
+    
     return response.text
 
-# Custom CSS for gradient background and centering the header
 custom_css = """
 <style>
 body {
@@ -28,29 +31,30 @@ h1 {
 </style>
 """
 
-st.set_page_config(page_title="Gemini LLM Demo", page_icon=":gem:")
+st.set_page_config(page_title="Gemini LLM Demo")
 st.markdown(custom_css, unsafe_allow_html=True)
 
 # Centered header
 st.title("GemCHAT")
 
 # Create a custom container for input
-input_container = st.container()
 
-with input_container:
-    st.text("How can I help you?")
-    input = st.text_input("", placeholder="Enter your problem here", key="input")
+st.text("How can I help you?")
+input = st.text_input("", placeholder="Query about the image", key="input")
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+image=""
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", width=500)
 
 # Create a custom container for the button
 button_container = st.container()
 
 with button_container:
-    submit = st.button("Send")
+    submit = st.button("Ask GemCHAT")
 
 # Display the response on top of the screen
 if submit:
-    output = get_response(input)
-    st.subheader("Here you go! ")
+    output = get_response(input,image)
+    st.subheader("Result")
     st.write(output)
-
-
